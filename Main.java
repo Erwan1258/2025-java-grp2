7293e446d463323550caa45846b316a4dde61a9c
erwan <erwanpronscri@icloud.com>
2025-02-11 11:18:59 +0100

    Mes modifications locales

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package exo;
 
 public class Main {
@@ -6,5 +7,35 @@ public class Main {
         compte1.retirer(500);
         compte1.deposer(300);
         compte1.retirer(3000);
+=======
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Demande des infos à l'utilisateur
+        Scanner scanner = new Scanner(System.in); // Création de l'objet Scanner
+        System.out.print("Entrez la couleur : ");
+        String couleur = scanner.nextLine();
+        scanner.close(); // Fermeture du scanner
+
+        boolean found = false;
+        for(String color : Voiture.colorAutorized) {
+            if (color.equals(couleur)) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            // Création de la voiture
+            Voiture voiture = new Voiture("Toyota", "Corolla", "Rouge");
+            
+            // Affichage des infos de la voiture
+            System.out.println(voiture.getMarque() + " " + voiture.getModele() + " " + voiture.getCouleur());
+        } else {
+            System.out.println("Erreur dans le choix de la couleur !");
+            System.exit(1);
+        }
+>>>>>>> d8c1a25 (Initial commit)
     }
 }
